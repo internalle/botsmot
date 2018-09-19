2f217dc14602e7d0aaa6bbcff9efa72bc887efc7
slobodan <internalle@gmail.com>
2018-09-19 21:04:12 +0200

    Add  fallback

--- routes/botman.php
+++ routes/botman.php
@@ -7,3 +7,7 @@ $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
 $botman->hears('.*(Play|Speak|play|speak).*', BotManController::class.'@startConversation');
+
+$botman->fallback(function($bot) {
+    $bot->reply('Sorry, I did not understand these commands. Here is a list of commands I understand: ...');
+});
