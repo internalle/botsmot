1157750ba9727489a6ca08e530d7fe500e975ecd
slobodan <internalle@gmail.com>
2018-09-29 17:14:20 +0200

    changes

--- routes/botman.php
+++ routes/botman.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 
 $botman->hears('Hi', function ($bot) {
-    Storage::put('GGGGGG.txt', 'Contents');
-    Storage::get('GGGGGG.txt');
+
+    $bot->reply(serialize($bot));
     $bot->reply('Hello!');
 });
 $botman->hears('.*(Play|Speak|play|speak).*', BotManController::class.'@startConversation');
