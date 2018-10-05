a17ee9b5e08667aef0197bc8654bb6cadd458b1e
slobodan <internalle@gmail.com>
2018-10-05 15:26:33 +0200

    remove log

--- routes/botman.php
+++ routes/botman.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 
 $botman->hears('Hi', function ($bot) {
-
-    $bot->reply(serialize($bot));
     $bot->reply('Hello!');
 });
 $botman->hears('.*(Play|Speak|play|speak).*', BotManController::class.'@startConversation');
